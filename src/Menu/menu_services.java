3478d86b239e4eeb8257e3e7e2c379374cbbee33
evelyn <evelyncc132002@gmail.com>
2023-02-28 12:56:13 +0000

    Fix: key_menu_crud

--- src/Menu/menu_services.java
+++ src/Menu/menu_services.java
@@ -19,8 +19,8 @@ public class menu_services {
 
 		String[] services = {"Diseño de logo", "Tarjeta de invitación", "Redes Sociales", "Salir" };
 		String[] crud = {"Create", "Read", "Update", "Delete", "Ordenar", "Back", "Salir"};
-		boolean key_menu_services, key_menu_invitation, key_menu_socialnetworks = false;
-		boolean key_menu_logo = false;
+		boolean key_menu_crud = false;
+		boolean key_menu_services = false; 
 		LogoDesign log = null;
 		Singleton.LogoDesign = new ArrayList <LogoDesign> ();
 
@@ -28,11 +28,13 @@ public class menu_services {
 
 			int option_menu_services = functions_menu.menubuttons(services, "Elige un servicio:", "Servicios");
 
+			key_menu_services = true;
+
 			switch (option_menu_services) {
 
 			case 0: // LOGO
 
-				key_menu_services = true;
+				key_menu_crud = true;
 
 				do {
 
@@ -42,22 +44,18 @@ public class menu_services {
 
 					case 0: // CREATE
 						create_functions.create_LogoDesign(log);
-						key_menu_logo = true;
 						break;
 
 					case 1: // READ
 						read_functions.read_logoDesign(log);
-						key_menu_logo = true;
 						break;
 
 					case 2: // UPDATE
 						update_functions.update_LogoDesign(log);
-						key_menu_logo = true;
 						break;
 
 					case 3:  // DELETE
 						delete_functions.delete_LogoDesign(log);
-						key_menu_logo = true;
 						break;
 
 					case 4: // ORDENAR
@@ -67,24 +65,24 @@ public class menu_services {
 
 					case 5: // ATRÁS
 						JOptionPane.showMessageDialog(null, "Back");
-						key_menu_logo = false;
+						key_menu_crud = false;
 						break;
 
 					default: // SALIR
 						JOptionPane.showMessageDialog(null, "Salir");
-						key_menu_logo = false;
+						key_menu_crud = false;
 						key_menu_services = false;
 						break;
 
 					}
 
-				}while (key_menu_logo == true);
+				}while (key_menu_crud != false);
 
 				break; 
 
 			case 1: // TARJETA DE INVITACIÓN
 
-				key_menu_services = true;
+				key_menu_crud = true;
 
 				do {
 
@@ -94,45 +92,47 @@ public class menu_services {
 
 					case 0: // CREATE
 						
-						key_menu_invitation = true;
+						key_menu_crud = true;
 						break;
 
 					case 1: // READ
 						JOptionPane.showMessageDialog(null, "Read");
-						key_menu_invitation = true;
 						break;
 
 					case 2: // UPDATE
 						JOptionPane.showMessageDialog(null, "Update");
-						key_menu_invitation = true;
 						break; 
 
 					case 3: // DELETE
 						JOptionPane.showMessageDialog(null, "Delete");
-						key_menu_invitation = true;
 						break;
 
-					case 4: // ATRÁS
+					case 4: // ORDENAR
+						JOptionPane.showMessageDialog(null, "Ordenar");
+						break;
+
+					case 5: // ATRÁS
 						JOptionPane.showMessageDialog(null, "Back");
-						key_menu_invitation = false;
+						key_menu_crud = false;
+						System.out.print("Volver");				
 						break;
 
 					default: // SALIR
 						JOptionPane.showMessageDialog(null, "Salir");
-						key_menu_invitation = false;
+						key_menu_crud = false;
 						key_menu_services = false;
-
+						System.out.print("Saliendo del todo");				
 						break; 
 
 					}
 
-				}while (key_menu_invitation == true);
+				}while (key_menu_crud != false);
 
 				break;
 
 			case 2: // REDES SOCIALES
 
-				key_menu_services = true;
+				key_menu_crud = true;
 
 				do {
 
@@ -142,39 +142,38 @@ public class menu_services {
 
 					case 0: // CREATE
 						JOptionPane.showMessageDialog(null, "Create");
-						key_menu_socialnetworks = true;
 						break;
 
 					case 1: // READ
 						JOptionPane.showMessageDialog(null, "Read");
-						key_menu_socialnetworks = true;
 						break;
 
 					case 2: // UPDATE
 						JOptionPane.showMessageDialog(null, "Update");
-						key_menu_socialnetworks = true;
 						break; 
 
 					case 3: // DELETE
 						JOptionPane.showMessageDialog(null, "Delete");
-						key_menu_socialnetworks = true;
 						break;
 
-					case 4: // ATRÁS
+					case 4: // ORDENAR
+						JOptionPane.showMessageDialog(null, "Ordenar");
+						break;
+
+					case 5: // ATRÁS
 						JOptionPane.showMessageDialog(null, "Back");
-						key_menu_socialnetworks = false;
+						key_menu_crud = false;
 						break;
 
 					default: // SALIR
 						JOptionPane.showMessageDialog(null, "Salir");
-						key_menu_socialnetworks = false;
+						key_menu_crud = false;
 						key_menu_services = false;
-
 						break; 
 
 					}
 
-				}while (key_menu_socialnetworks == true); 
+				}while (key_menu_crud != false); 
 
 				break;
 
@@ -184,7 +183,7 @@ public class menu_services {
 				break;
 			}
 
-		}while (key_menu_services == true);
+		}while (key_menu_services != false);
 
 	}
 }
